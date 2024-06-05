aec1ebd430803588f4781c302da7ff327ee1f8e2
catcat0902 <kshiloveureal@gmail.com>
2024-06-05 22:48:44 +0900

    제리 등장~

--- oss_pp_phase1/main.py
+++ oss_pp_phase1/main.py
@@ -9,7 +9,7 @@ screen_width = 480
 screen_height = 640
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption("똥피하기")
+pygame.display.set_caption("톰을 피해라!!")
 clock = pygame.time.Clock()
 FPS = 60
 
@@ -23,6 +23,13 @@ tom_images = [
     pygame.image.load(r"C:\Users\김서현\Desktop\pygame_basic\tom3.png").convert_alpha()
 ]
 
+# 캐릭터 크기 및 위치 설정
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width - character_width) / 2
+character_y_pos = screen_height - character_height
+
 # 게임 루프
 running = True
 while running:
@@ -32,6 +39,7 @@ while running:
 
     # 화면에 그리기
     screen.blit(background, (0, 0))  # 배경 그리기
+    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
 
     pygame.display.flip()  # 화면 업데이트 (전체 코드에서는 flip() 사용)
     clock.tick(FPS)  # FPS 제한
